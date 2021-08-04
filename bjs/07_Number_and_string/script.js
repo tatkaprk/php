93a1b93e2e17abab57603d82a85d11991adad608
tatkaprk <83012727+tatkaprk@users.noreply.github.com>
2021-08-04 18:22:50 +0200

    initial commit

--- bjs/07_Number_and_string/script.js
+++ bjs/07_Number_and_string/script.js
@@ -3,6 +3,91 @@ let operation = null;
 
 const inputWindow = document.getElementById('inputWindow');
 
+document.querySelectorAll('#btn_num').forEach((button)=>{
+    button.addEventListener('click', function (e) {
+        inputWindow.value +=e.target.textContent;
+    })
+});
+
+
+// document.getElementById('btn_num').addEventListener('click', function (e) {
+//     console.log(e.target.textContent);
+//     inputWindow.value +="1";
+
+
+// })
+
+document.getElementById('btn_sum').addEventListener('click', function () {
+    lastOperand = parseInt (inputWindow.value);
+    operation = "sum";
+    inputWindow.value = '';
+})
+
+document.getElementById('btn_def').addEventListener('click', function () {
+    lastOperand = parseInt (inputWindow.value);
+    operation = "def";
+    inputWindow.value = '';
+})
+
+document.getElementById('btn_multiply').addEventListener('click', function () {
+    lastOperand = parseInt (inputWindow.value);
+    operation = "multiply";
+    inputWindow.value = '';
+})
+
+document.getElementById('btn_div').addEventListener('click', function () {
+    lastOperand = parseInt (inputWindow.value);
+    operation = "div";
+    inputWindow.value = '';
+})
+
+document.getElementById('btn_sqr').addEventListener('click', function () {
+    operandValue =  parseInt (inputWindow.value);
+    lastOperand = isNaN(operandValue)?0:operandValue;
+    operation = "sqr";
+    inputWindow.value = '';
+})
+
+
+
+document.getElementById('btn_calc').addEventListener('click', function () {
+    if (operation==="sum"){
+        const result = lastOperand + parseInt (inputWindow.value);
+        operation = null;
+        lastOperand = 0;
+        inputWindow.value = result;
+    }
+    if (operation==="def"){
+        const result = lastOperand - parseInt (inputWindow.value);
+        operation = null;
+        lastOperand = 0;
+        inputWindow.value = result;
+    }
+    if (operation==="multiply"){
+        const result = lastOperand * parseInt (inputWindow.value);
+        operation = null;
+        lastOperand = 0;
+        inputWindow.value = result;
+    }
+    if (operation==="div"){
+        const result = lastOperand / parseInt (inputWindow.value);
+        operation = null;
+        lastOperand = 0;
+        inputWindow.value = result;
+    }
+    if (operation==="sqr"){
+        let result;
+        if (lastOperand == 0) {
+            result = Math.sqrt(parseInt (inputWindow.value));
+        }
+        else {
+            result = Math.sqrt(lastOperand);
+        }
+        operation = null;
+        lastOperand = 0;
+        inputWindow.value = result;
+    }
+})
 
 document.getElementById('btn_clr').addEventListener('click', function () {
     lastOperand = 0;
